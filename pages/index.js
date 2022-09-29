115c7020f00ae6ed7063665996b3317f1a639fe4
anikjoyy <anikjoy.cse@gmail.com>
2022-09-29 23:42:46 +0600

    added a notification to contact submission and implemented react portal

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Fragment } from 'react';
 
 import FeaturedPosts from '../components/home-page/featured-posts';
@@ -7,6 +8,13 @@ import { getFeaturedPosts } from '../lib/posts-util';
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>ANIK Blog</title>
+        <meta
+          name='descriptrion'
+          content='I post blog about programming and web development'
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
